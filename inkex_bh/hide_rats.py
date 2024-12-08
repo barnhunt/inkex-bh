barnhunt/inkex-bh

commit 5c7f96471805dda54a7202f50ce1e760f5adf97b

feat!: drop support for python 3.7, test under python 3.12
Inkscape 1.0 ships with python 3.8, so we should be okay in that regard.

inkex_bh/hide_rats.py
@@ -22,8 +22,10 @@ from argparse import ArgumentParser
 from functools import reduce
 from operator import add
 from types import MappingProxyType
+from typing import Final
 from typing import Iterable
 from typing import Iterator
+from typing import Literal
 from typing import Sequence
 
 import inkex
@@ -32,8 +34,6 @@ from inkex.localization import inkex_gettext as _
 from . import debug
 from . import typing as types
 from ._compat import compose_transforms
-from ._compat import Final
-from ._compat import Literal
 from ._compat import to_dimensionless
 from .constants import BH_RAT_GUIDE_MODE
 from .constants import BH_RAT_PLACEMENT

.github/workflows/tests.yml
@@ -5,7 +5,7 @@ on:
   - pull_request
 
 env:
-  'default-python-version': '3.10'
+  'default-python-version': '3.12'
 
 jobs:
   tests:
@@ -15,16 +15,16 @@ jobs:
       fail-fast: false
       matrix:
         os: ['ubuntu-latest']
-        python-version: ['3.7', '3.8', '3.9', '3.10', '3.11']
+        python-version: ['3.8', '3.9', '3.10', '3.11', '3.12']
         include:
           - os: 'windows-latest'
-            python-version: '3.10'
+            python-version: '3.12'
             install-inkscape: true
           - os: 'macos-latest'
-            python-version: '3.10'
+            python-version: '3.12'
             install-inkscape: true
 
-          - python-version: '3.10'
+          - python-version: '3.12'
             install-inkscape: true
 
     steps:

inkex_bh/_compat.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import sys
-
 import inkex
 
 from . import typing as types
@@ -10,19 +8,7 @@ __all__ = [
     "compose_transforms",
     "ensure_str",
     "to_dimensionless",
-    "Final",
-    "Literal",
-    "TypedDict",
 ]
-
-if sys.version_info >= (3, 8):
-    from typing import Final
-    from typing import Literal
-    from typing import TypedDict
-else:
-    from typing_extensions import Final
-    from typing_extensions import Literal
-    from typing_extensions import TypedDict
 
 
 def ensure_str(s: bytes | str, encoding: str = "utf-8", errors: str = "strict") -> str:

inkex_bh/create_inset.py
@@ -27,11 +27,11 @@ from typing import Callable
 from typing import Iterable
 from typing import Iterator
 from typing import Sequence
+from typing import TypedDict
 
 import inkex
 from inkex.localization import inkex_gettext as _
 
-from ._compat import TypedDict
 from .constants import BH_INSET_EXPORT_ID
 from .constants import BH_INSET_VISIBLE_LAYERS
 from .workarounds import monkeypatch_inkscape_command_for_appimage

pyproject.toml
@@ -3,7 +3,7 @@ name = "inkex-bh"
 description = "Inkscape extensions for Barn Hunt"
 keywords = ["inkscape", "barn hunt", "barnhunt"]
 license = {text = "GNU GPLv3"}
-requires-python = ">=3.7"
+requires-python = ">=3.8"
 classifiers = [
     "Development Status :: 4 - Beta",
     "Environment :: Plugins",
@@ -21,7 +21,6 @@ dynamic = ["version", "readme"]
 dependencies = [
     # FIXME: not installable from PyPI?
     # "inkex",
-    "typing_extensions; python_version<'3.8'",
     "lxml",
 ]
 
@@ -89,7 +88,7 @@ extra-dependencies = [
     # Convenience/dev
     "ruff==0.6.3",
 
-    # Running mypy requires python >= 3.8
+    # mypy
     "mypy==1.11.2",
     "types-lxml",
     "hatchling",

tox.ini
@@ -2,17 +2,17 @@
 minversion = 3.3
 isolated_build = true
 envlist =
-  py{37,38,39,310,311}-inkex{10,11,12,13}
+  py{38,39,310,311,312}-inkex{10,11,12,13}
   cover-{clean,report}
   lint
 
 [gh-actions]
 python =
-    3.7: py37-inkex{10,11,12}
-    3.8: py38-inkex{10,11,12,13}
-    3.9: py39-inkex{10,11,12,13}
-    3.10: py310-inkex{10,11,12,13}
-    3.11: py311-inkex{10,11,12,13}
+    3.8: py38
+    3.9: py39
+    3.10: py310
+    3.11: py311
+    3.12: py312
 
 [testenv]
 commands =
@@ -44,8 +44,8 @@ deps =
     hatch
 
 depends =
-  py{37,38,39,310,311}: cover-clean
-  cover-report:   {py37,py38,py39,py310,py311}-inkex{10,11,12,13}
+  py{38,39,310,311,312}: cover-clean
+  cover-report: py{38,39,310,311,312}-inkex{10,11,12,13}
 
 
 [testenv:cover-clean]
@@ -64,7 +64,6 @@ commands =
 [testenv:lint]
 skip_install = true
 deps =
-    # NB: Running mypy requires python_version >= 3.8
     mypy==1.11.2
     types-lxml
 

commit 85e6c365ab648cd8d90080173faf090f3bca0902

fix(hide_rats): fix unstyled guide-layer rectangles
This fixes a bug introduced in 5c7f964.

inkex_bh/hide_rats.py
@@ -21,7 +21,6 @@ import re
 from argparse import ArgumentParser
 from functools import reduce
 from operator import add
-from types import MappingProxyType
 from typing import Final
 from typing import Iterable
 from typing import Iterator
@@ -115,26 +114,22 @@ class RatGuide:
         self._add_rect(bbox, "exclusion")
         self.exclusions.append(bbox)
 
-    DEFAULT_STYLE: Final = MappingProxyType(
-        {
-            "fill": "#c68c8c",
-            "fill-opacity": "0.125",
-            "stroke": "#ff0000",
-            "stroke-width": "1",
-            "stroke-opacity": "0.5",
-            "stroke-dasharray": "2,6",
-            "stroke-linecap": "round",
-            "stroke-miterlimit": "4",
-        }
-    )
-    STYLES: Final = MappingProxyType(
-        {
-            "notation": {
-                **DEFAULT_STYLE,
-                "fill": "#aaaaaa",
-            }
-        }
-    )
+    DEFAULT_STYLE: Final = {
+        "fill": "#c68c8c",
+        "fill-opacity": "0.125",
+        "stroke": "#ff0000",
+        "stroke-width": "1",
+        "stroke-opacity": "0.5",
+        "stroke-dasharray": "2,6",
+        "stroke-linecap": "round",
+        "stroke-miterlimit": "4",
+    }
+    STYLES: Final = {
+        "notation": {
+            **DEFAULT_STYLE,
+            "fill": "#aaaaaa",
+        },
+    }
 
     def _add_rect(self, bbox: inkex.BoundingBox, mode: GuideMode) -> None:
         rect = inkex.Rectangle.new(bbox.left, bbox.top, bbox.width, bbox.height)